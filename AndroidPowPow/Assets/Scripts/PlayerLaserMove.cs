1ec9fb22c5ff41ce537d099e00d88a4b56e01a51
Deanard <amkvissers@gmail.com>
2017-07-29 16:23:25 -0300

    Player Move & Bullets

---
+++ AndroidPowPow/Assets/Scripts/PlayerLaserMove.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class PlayerLaserMove : MonoBehaviour {
+
+	Vector2 trajec;
+	int new_proj = 0;
+	protected Rigidbody2D rigBod;
+	List<Collider> colliders = new List<Collider>();
+
+	// Use this for initialization
+	void OnEnable() {
+
+		rigBod = GetComponent<Rigidbody2D> ();
+			
+	}
+
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void FixedUpdate () {
+
+		trajec.x = 3; //new_proj;
+		rigBod.position = rigBod.position + trajec;
+
+
+	}
+	void OnTriggerEnter2D (Collider2D collision) {
+	
+		if (collision.gameObject.tag == "BulletKill")
+			Destroy (this.gameObject);
+
+	}
+}
---
+++ AndroidPowPow/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+	public float minGroundNormalY = .65f;
+
+	protected Vector2 moves;
+	protected Rigidbody2D rigBod;
+	//public GameObject playerLaser;
+	protected ContactFilter2D cntcFltr;
+	protected RaycastHit2D[] hitBuffer = new RaycastHit2D[16];
+	protected List<RaycastHit2D> hitBufferList = new List<RaycastHit2D> (16);
+
+	protected const float minMoveDistance = 0.001f;
+	protected const float shellRadius = 0.01f;
+	// Use this for initialization
+
+	void OnEnable() {
+
+		rigBod = GetComponent<Rigidbody2D> ();
+
+	}
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if ((Input.GetMouseButtonDown (0))) {
+
+			GameObject laserProj = (GameObject)Instantiate (Resources.Load ("PlayerLaser"));
+			laserProj.transform.position = rigBod.position;
+
+		}
+
+	}
+
+	void FixedUpdate() {
+
+		moves = Vector2.zero;
+
+
+
+		if( Input.GetKey("up") || Input.GetKey("w") )
+			moves.y += 2;
+				
+		if( (Input.GetKey("down")) || (Input.GetKey("s")) )
+			moves.y -= 2;
+
+				
+		if( (Input.GetKey("left")) || (Input.GetKey("a")) )
+			moves.x -= 2;
+					
+		if( (Input.GetKey("right")) || (Input.GetKey("d")) )
+			moves.x += 2;
+	
+						
+		moves = moves / 2;
+
+		Movement (moves);
+
+	}
+
+	void Movement(Vector2 moveing) {
+
+		rigBod.position = rigBod.position + moveing;
+
+	}
+}
