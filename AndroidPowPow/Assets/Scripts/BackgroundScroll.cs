29e1990922f9e447dfafec7f4dda8af9ca871b2d
Erik Olaf Manahan <eomanaha@uwaterloo.ca>
2017-07-30 01:07:21 -0300

    Moving backgrounds

---
+++ AndroidPowPow/Assets/Scripts/BackgroundScroll.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundScroll : MonoBehaviour {
+
+	public float speed = 1f;
+	public float reposition;
+
+	// Use this for initialization
+	void Start () {
+		float startingOffset;
+		SpriteRenderer sprite;
+		sprite = this.GetComponent<SpriteRenderer> ();
+		startingOffset = this.transform.position.x;
+		if (startingOffset > sprite.bounds.size.x)
+			reposition = startingOffset;
+		else
+			reposition = startingOffset + sprite.bounds.size.x;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.transform.Translate (new Vector3 (-1 * speed, 0));
+	}
+
+	void OnBecameInvisible () {
+		this.transform.position = new Vector3 (reposition, 0);
+	}
+}
